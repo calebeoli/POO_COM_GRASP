1c2c36d363f02646947bcecdf26b6bd5bb4b8365
Felipech09 <felipecamhipolito@gmail.com>
2025-11-11 22:02:12 -0300

    Atualiza LojaController e adiciona Itnes.java

--- src/Controller/LojaController.java
+++ src/Controller/LojaController.java
@@ -1,45 +1,74 @@
+package Controller;
+
+import java.util.List;
+
+import Model.CarrinhoDeCompras;
+import Model.Itnes;
+import Model.Produto;
+import View.View;
+
 public class LojaController {
-  private CarinhoDeCompras Model;
-  private View view;
+    private CarrinhoDeCompras carrinho;
+    private View view;
+    private Itnes catalogo;
 
-  public iniciar() {
-    int opcao;
-    opcao = view.exibirMnuPrincipal();
-    switch (opcao) {
-      case 1 adicionarProduto();
-      case 2 verCarrinho();
-      case 3 finalizarCompra();
-      case 0 {
-        if (confirmarSaida()) {
-          view.exibirMensagem("fechando a loja");
-          return;
-        }
-      }
-        default view.exibirMensagem("Opção inválida");
+    public LojaController(View view, CarrinhoDeCompras carrinho) {
+        this.view = view;
+        this.carrinho = carrinho;
+        this.catalogo = new Itnes();
     }
-  }
-}
-private boolean confirmarSaida() {
-  String resposta = view.lerEntrada("Tem certeza que deseja sair? (sim/não): ");
-  return resposta.equals("sim");
-}
 
-private void adicionarProduto (){
-  int escolha = view.exibirMenuProdtos(produtos);
-  if (escolha > 0 && escola <= produtos.size()) {
-    Produto p = produtos.get(eescolha - 1);
-    carrinho.adicionarProduto(p);
-    view.exibirMensagem("'" + p.getNome() + "'adicionado ao carrinho");
-  } else if (escolha != 0) {
-    view.exibirMensagem("produto inválido");
-  }
-}
+    public void iniciar() {
+        int opcao;
 
-private void verCarrinho() {
-  view.exibirCarrinho(carrinho.getItens(), carrinho.calcularTotal());
-}
+        while (true) {
+            opcao = view.ExibeMenu();
 
-private void finalizarCompra() {
-  view.eibirNotaFiscal(carrinho.getItens(), carrinho.calcularTotal());
-  }
+            switch (opcao) {
+                case 1:
+                    adicionarProduto();
+                    break;
+                case 2:
+                    verCarrinho();
+                    break;
+                case 3:
+                    finalizarCompra();
+                    break;
+                case 0:
+                    if (confirmarSaida()) {
+                        view.exibirMensagem("fechando a loja");
+                        return;
+                    }
+                    break;
+                default:
+                    view.exibirMensagem("Opção inválida");
+            }
+        }
+    }
+
+    private boolean confirmarSaida() {
+        String resposta = view.lerEntrada("Tem certeza que deseja sair? (sim/não): ");
+        return resposta.equals("sim");
+    }
+
+    private void adicionarProduto (){
+        List<Produto> produtosDisponiveis = catalogo.getProdutosDisponiveis(); 
+        int escolha = view.exibirMenuProdutos(produtosDisponiveis);
+        
+        if (escolha > 0 && escolha <= produtosDisponiveis.size()) {
+            Produto p = produtosDisponiveis.get(escolha - 1);
+            carrinho.AdicionaCarrionho(p); 
+            view.exibirMensagem("'" + p.getNome() + "'adicionado ao carrinho");
+        } else if (escolha != 0) {
+            view.exibirMensagem("produto inválido");
+        }
+    }
+
+    private void verCarrinho() {
+        view.exibirCarrinho(carrinho.getItens(), carrinho.CalculaTotal());
+    }
+
+    private void finalizarCompra() {
+        view.eibirNotaFiscal(carrinho.getItens(), carrinho.CalculaTotal());
+    }
 }
---
+++ src/Model/Itnes.java
@@ -0,0 +1,25 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Itnes { 
+    private List<Produto> produtos;
+
+    public Itnes () { 
+        this.produtos = new ArrayList<>();
+        this.produtos.add(new Produto("Notebook Core i5", 3500.00));
+        this.produtos.add(new Produto("Smartphone Última Geração", 2100.00));
+        this.produtos.add(new Produto("Mouse Pad Grande", 50.00));
+        this.produtos.add(new Produto("Fone de ouvido", 80.00));
+        this.produtos.add(new Produto("HD Externo 1TB", 300.00));
+        this.produtos.add(new Produto("Teclado gamer", 120.00));
+        this.produtos.add(new Produto("Filtro de Linha 8 Tomadas", 85.00));
+        this.produtos.add(new Produto("Memória RAM DDR5 16GB", 380.00));
+    }
+
+    public List<Produto> getProdutosDisponiveis() {
+        return Collections.unmodifiableList(produtos);
+    }
+}
